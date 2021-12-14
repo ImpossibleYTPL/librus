ea90cf2c2c84ea2f7f05e8f6a61b289946ed4897
ImpossibleYTPL <77750579+ImpossibleYTPL@users.noreply.github.com>
2021-12-14 18:39:51 +0100

    szablon

---
+++ main copy.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Librus</title>
+    <link rel="stylesheet" href="./Styles/style.css">
+</head>
+<body>
+    <div id="baner">
+        <div id="logo">
+            <img src="logo.png" alt="logo">
+        </div>
+        <div id="bCenter">
+            okok
+        </div>
+        <div id="bRight">
+            okok
+        </div>
+    </div>
+    <div id="main">
+        
+    </div>
+    <div id="stopka">
+        <hr />
+
+    </div>
+</body>
+</html>
