2f8549fb9b0f77f9d43cbd0ae0ed306dd38fc3b6
ImpossibleYTPL <77750579+ImpossibleYTPL@users.noreply.github.com>
2021-12-15 08:25:08 +0100

    ko

--- uczen/uczen.php
+++ uczen/uczen.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Librus</title>
-    <link rel="stylesheet" href="./Styles/style.css?v=1.1">
+    <link rel="stylesheet" href="./Styles/uczen.css?v=1.1">
 </head>
 <body>
     <?php
@@ -22,6 +22,9 @@
             <a href="#">Pomoc</a>
             <a href="#">Wyloguj</a>
         </div>
+    </div>
+    <div id="baner2">
+
     </div>
     <div id="main">
         
